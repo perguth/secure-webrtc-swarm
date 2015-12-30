8796db2dcb2257e0270224a422229de4417f1e41
Per Guth <mail@perguth.de>
2015-12-30 21:30:13 +0100

    added secure key exchange, laid the basis for encryption

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 var debug = require('debug')('secure-webrtc-swarm')
 var webrtcSwarm = require('./lib/mod-webrtc-swarm')
 var signalhub = require('signalhub')
+var nacl = require('tweetnacl')
 
 /** Test using:
 
 // Browser tab A
-var a = s('localhost:7000', {uuid: '03s55f2bdn79pciis2wqub000',
-  extendedInvites: { signPubKey: 'signPrivKey' }
+// privKey(A) = '4oDfzsyer+vZg/SouHNiYov+OqzlcJoa1WNO+Zu+K+o='
+var a = s('localhost:7000', {
+  uuid: 'HQpECMWIUaOIvdTLzmvfFN1CZMSFWAOmJ1pCEYCr5zA=',
+  extendedInvites: {
+    '447aTPffUY0o9YKYGq9iHlooC9R4y2hCSv93b1/duIE=': // <- signPubKey(AB)
+    'upA4zHBTedolBKZ6f5TRe+TZcSfPAW1KVfUB2xFjNmDjjtpM999RjSj1gpgar2IeWigL1HjLaEJK/3dvX924gQ==' // <- signPrivKey(AB)
+  }
 })
 
 // Browser tab B
-var b = s('localhost:7000', {uuid: 'ciis2wu1f00003s5560m3fzzc',
-  whitelist: ['03s55f2bdn79pciis2wqub000'],
-  receivedInvites: { '03s55f2bdn79pciis2wqub000': 'signPrivKey' }
+// privKey(B) = 'V6j8zDQOZlRV/HtSNWxel708fe0IHXJWsvhAtgNwSeU='
+var b = s('localhost:7000', {
+  uuid: 'wEXhh5BleF626PHXURkRxMD4jlBO9ohkuGVCPb9AaFU=',
+  whitelist: ['HQpECMWIUaOIvdTLzmvfFN1CZMSFWAOmJ1pCEYCr5zA='],
+  receivedInvites: {
+    'HQpECMWIUaOIvdTLzmvfFN1CZMSFWAOmJ1pCEYCr5zA=': // <- pubKey(A)
+    'upA4zHBTedolBKZ6f5TRe+TZcSfPAW1KVfUB2xFjNmDjjtpM999RjSj1gpgar2IeWigL1HjLaEJK/3dvX924gQ==' // <- signPrivKey(AB)
+  }
 })
 
 */
 
+window.nacl = nacl
 window.s =
 module.exports = function (hub, opts) {
   opts = opts || {}
@@ -31,16 +43,49 @@ module.exports = function (hub, opts) {
   var me = opts.uuid
   if (me) swarm.whitelist.push(me)
 
+  function sign (data, signPrivKey) {
+    signPrivKey = nacl.util.decodeBase64(signPrivKey)
+    data = nacl.util.decodeUTF8(data)
+    data = nacl.sign.detached(data, signPrivKey)
+    data = nacl.util.encodeBase64(data)
+    return data
+  }
+  function deriveSignPubKey (signPrivKey) {
+    var signPubKey = nacl.sign.keyPair.fromSecretKey(
+      nacl.util.decodeBase64(signPrivKey)
+    ).publicKey
+    signPubKey = nacl.util.encodeBase64(signPubKey)
+    return signPubKey
+  }
+  function encrypt (data, nonce, theirPubKey, myPrivKey) {
+    nonce = nacl.util.decodeBase64(nonce)
+    theirPubKey = nacl.util.decodeBase64(theirPubKey)
+    myPrivKey = nacl.util.decodeBase64(myPrivKey)
+    data = nacl.util.decodeUTF8(data)
+    data = nacl.box(data, nonce, theirPubKey, myPrivKey)
+    data = nacl.util.encodeBase64(data)
+    return data
+  }
+
   function wrap (data, channel) {
     if (channel === 'all') return data
 
     if (swarm.receivedInvites[channel]) {
       debug('sending ratcheting data to', channel)
-      var signPubKey = (swarm.receivedInvites[channel] === 'signPrivKey')
-        ? 'signPubKey' : false // mockup
+
+      var signPrivKey = swarm.receivedInvites[channel]
+      var signPubKey = deriveSignPubKey(signPrivKey)
+      var signature = sign(me, signPrivKey)
       data.signPubKey = signPubKey
-      data.signature = 'signature'
-      // encrypt/sign
+      data.signature = signature
+
+      var nonce = nacl.util.encodeBase64(nacl.randomBytes(24))
+      var theirPubKey = channel
+      var myPrivKey = opts.privKey
+      var signal = JSON.stringify(data.signal)
+      data.nonce = nonce
+      data.signal = encrypt(signal, nonce, theirPubKey, myPrivKey)
+
       return data
     }
 
@@ -49,6 +94,24 @@ module.exports = function (hub, opts) {
     }
 
     // encrypt/sign
+    return data
+  }
+
+  function verify (data, signature, signPubKey) {
+    signature = nacl.util.decodeBase64(signature)
+    signPubKey = nacl.util.decodeBase64(signPubKey)
+    data = nacl.util.decodeUTF8(data)
+    var success = nacl.sign.detached.verify(data, signature, signPubKey)
+    return success
+  }
+  function decrypt (data, nonce, theirPubKey, myPrivKey) {
+    nonce = nacl.util.decodeBase64(nonce)
+    theirPubKey = nacl.util.decodeBase64(theirPubKey)
+    myPrivKey = nacl.util.decodeBase64(myPrivKey)
+    data = nacl.util.decodeBase64(data)
+    data = nacl.box.open(data, nonce, theirPubKey, myPrivKey)
+    if (!data) return false
+    data = nacl.util.encodeUTF8(data)
     return data
   }
 
@@ -73,16 +136,27 @@ module.exports = function (hub, opts) {
       return false
     }
 
-    var verified = (data.signature === 'signature') // mockup
+    var verified = verify(data.from, data.signature, data.signPubKey)
     if (!verified) {
       debug('signature invalid - dropping offered pubKey', data)
       return false
     }
 
-    // decrypt/verify
-    debug('received and verified pubKey')
+    var theirPubKey = data.from
+    var myPrivKey = opts.privKey
+    data.signal = decrypt(data.signal, data.nonce, theirPubKey, myPrivKey)
+
+    if (!data.signal) {
+      debug('verification while decrypting failed', data)
+      return false
+    }
+
+    data.signal = JSON.parse(data.signal)
+    debug('received and verified pubKey of invitee')
+
     delete swarm.extendedInvites[data.signPubKey]
     swarm.whitelist.push(data.from)
+
     return data
   }
 
